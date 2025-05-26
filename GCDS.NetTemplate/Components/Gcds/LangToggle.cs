3f85e0958ec38e441561ea6aeeffb32ca4fc44aa
Shaun Laughland <22419216+s-laugh@users.noreply.github.com>
2025-05-26 11:24:43 -0400

    adding GCDS required components for ease of custom implementations

---
+++ GCDS.NetTemplate/Components/Gcds/LangToggle.cs
@@ -0,0 +1,15 @@
+﻿namespace GCDS.NetTemplate.Components.Gcds
+{
+    /// <summary>
+    /// Implements GCDS Language toogle component.
+    /// Not used by default in templates, but avaliable as a required component for custom implementations ease of use.
+    /// https://design-system.alpha.canada.ca/en/components/language-toggle/
+    /// </summary>
+    public class LangToggle : Common
+    {
+        /// <summary>
+        /// Link to tigger the language switch
+        /// </summary>
+        public required string Href { get; set; }
+    }
+}
